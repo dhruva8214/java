70bcc4cd37fb20d5697d90df85339dacbc7f5c90
dhruva8214 <dhruva.m@cmr.edu.in>
2026-02-03 08:15:58 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -31,7 +31,7 @@ public class Test {
     // Main method
     public static void main(String[] args) {
         int x = 589;
-        int y = 10;
+        int y = 11;
 
         add(x, y);
         sub(x, y);
